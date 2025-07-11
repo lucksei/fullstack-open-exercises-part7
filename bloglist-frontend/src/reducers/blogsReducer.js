8178c6cd9f357ba37929bd1853cb5a9c602edccf
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-07-11 08:58:05 -0300

    worked on the blogsReducer further, not yet tested nor implemented in code

--- bloglist-frontend/src/reducers/blogsReducer.js
+++ bloglist-frontend/src/reducers/blogsReducer.js
@@ -9,19 +9,21 @@ const blogsSlice = createSlice({
       state.push(action.payload)
     },
     deleteBlog(state, action) {
-      // ...
+      const id = action.payload.id
+      return state.filter((b) => b.id === id ? false : true)
     },
     editBlog(state, action) {
-      // ...
+      const id = action.payload.id
+      return state.map((b) => b.id === id ? action.payload : b)
     },
     setBlogs(state, action) {
-      state = action.payload
+      return action.payload
     },
   },
 })
 
 
-export const { } = blogsSlice.actions
+export const { addBlog, deleteBlog, editBlog, setBlog } = blogsSlice.actions
 
 export const initializeBlogs = () => {
   return async (dispatch) => {
@@ -39,20 +41,24 @@ export const createBlog = (blog) => {
 
 export const upvoteBlog = (blog) => {
   return async (dispatch) => {
-    const votedBlog = { ...votedBlog, votes: blog.votes + 1 }
-    const blog = await blogService.update(blog)
+    const upvotedBlog = { ...blog, votes: blog.votes + 1 }
+    await blogService.update(upvotedBlog)
+    dispatch(editBlog(upvotedBlog))
   }
 }
 
 export const downvoteBlog = (blog) => {
   return async (dispatch) => {
-    // ...
+    const downvotedBlog = { ...blog, votes: blog.votes - 1 }
+    await blogService.update(downvotedBlog)
+    dispatch(editBlog(downvotedBlog))
   }
 }
 
-export const deleteBlog = (blog) => {
-  return (dispatch) => {
-    // ...
+export const removeBlog = (blog) => {
+  return async (dispatch) => {
+    await blogService.remove(blog.id)
+    dispatch(deleteBlog(blog))
   }
 }
 
