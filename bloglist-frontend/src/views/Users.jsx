94d276c5da0de277c56a65a577b906571cc05673
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-07-22 19:56:12 -0300

    added Users view -- exercise 7.14 done

--- bloglist-frontend/src/views/Users.jsx
+++ bloglist-frontend/src/views/Users.jsx
@@ -5,16 +5,37 @@ import { useSelector } from 'react-redux';
 const Users = () => {
   const blogs = useSelector((state) => state.blogs);
 
-  const result = _(blogs)
-    .groupBy('user')
+  const users = _(blogs)
+    .groupBy('user.username')
     .map((items, user) => {
+      console.log(user);
       return {
-        user,
+        user: user,
         blogCount: items.length,
       };
-    });
-  console.log(result);
-  return <div>Users</div>;
+    })
+    .value();
+
+  console.log(users);
+  return (
+    <>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <th>User</th>
+          <th>blogs created</th>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.user}>
+              <td>{user.user}</td>
+              <td>{user.blogCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Users;
